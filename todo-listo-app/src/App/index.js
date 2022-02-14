4968491b2362c7383eefdecd90c2b5a5f6f7ffbd
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2022-02-14 17:51:57 -0300

    Persiste en localStorage usando un CustomHook

--- todo-listo-app/src/App/index.js
+++ todo-listo-app/src/App/index.js
@@ -7,11 +7,38 @@ const todosDefault = [
   { text: 'Ver un curso de finanzas', completed: true }
 ]
 
+function useLocalStorage(itemName, initialValue) {
+  console.log("Entro acá");
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if(!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  }
+
+  return [
+    item,
+    saveItem
+  ]
+}
+
 function App() {
-  const [todos, setTodos] = useState(todosDefault);
+  const [todos, setTodos] = useLocalStorage('TODO_LIST', todosDefault);
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
   const [searchValue, setSearchValue] = useState('');
+
   let searchedTodos = [];
   
   if(searchValue.length <= 1) {
